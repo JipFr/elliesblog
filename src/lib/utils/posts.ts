ebe1824e40b8f8281ca059e24757ecec7bda00a4
Jip Fr <jipfrijlink@gmail.com>
2023-11-11 02:57:09 +0100

    AAAAAA

--- src/lib/utils/posts.ts
+++ src/lib/utils/posts.ts
@@ -1,5 +1,7 @@
-import fs from "fs";
-import marked from "marked";
+import fs from "node:fs";
+import { Marked } from "marked";
+
+const marked = new Marked();
 
 interface PostMeta {
 	title: string;
@@ -15,30 +17,32 @@ export interface Post {
 	html: string;
 }
 
-export function getPosts() {
+export async function getPosts() {
 	const postsPath = "./content/posts/";
 	const paths = fs.readdirSync(postsPath);
-	const posts: Post[] = paths
-		.map((path: string) => {
-			const content = fs.readFileSync(postsPath + path, "utf-8"); // Get markdown files
-			const splitContent = content.split("---");
-			const meta = Object.fromEntries(
-				splitContent[1]
-					.split("\n")
-					.filter(Boolean)
-					.map((t: string) => t.split(": "))
-			) as PostMeta;
-			const md = splitContent.slice(2).join("---").trim();
-			const post: Post = {
-				meta,
-				md,
-				html: marked.parse(md),
-			};
-			return post;
-		})
-		.sort((a: Post, b: Post) => {
-			return new Date(b.meta.date).getTime() - new Date(a.meta.date).getTime();
-		});
+	const posts: Post[] = (
+		await Promise.all(
+			paths.map(async (path: string) => {
+				const content = fs.readFileSync(postsPath + path, "utf-8"); // Get markdown files
+				const splitContent = content.split("---");
+				const meta = Object.fromEntries(
+					splitContent[1]
+						.split("\n")
+						.filter(Boolean)
+						.map((t: string) => t.split(": "))
+				) as PostMeta;
+				const md = splitContent.slice(2).join("---").trim();
+				const post: Post = {
+					meta,
+					md,
+					html: await marked.parse(md),
+				};
+				return post;
+			})
+		)
+	).sort((a: Post, b: Post) => {
+		return new Date(b.meta.date).getTime() - new Date(a.meta.date).getTime();
+	});
 
 	return posts;
 }
